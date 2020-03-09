252c11590cb6215809e0760da627157687522afb
Vitali Akulovich <vitali.akulovich.firefly@gmail.com>
2020-03-09 18:54:46 +0300

    Feature: add constants

---
+++ src/constants.js
@@ -0,0 +1,17 @@
+export const PHOTO = {
+  CAT_ALT: 'Cat',
+};
+
+export const BUTTONS = {
+  STORE_BUTTON: {
+    DELETE: 'X',
+    RESTORE: 'R',
+  },
+};
+
+export const URLS = {
+  BASE_PATH: 'https://mrsoft.by/tz20',
+  ROUTES: {
+    CATS_LIST: '/list.json',
+  },
+};
